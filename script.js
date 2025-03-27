4bc0262ca78538368e9e96a351a9fd057a6f9ee6
laubap <131703039+laubap@users.noreply.github.com>
2025-03-27 13:09:48 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function adicionarMatricula() {
     let container = document.getElementById("matriculas-container");
-
-    // Criar o div para o novo input
     let div = document.createElement("div");
     div.className = "input-group mt-2";
 
-    // Criar o input de matrícula
     let novoInput = document.createElement("input");
     novoInput.type = "text";
-    novoInput.name = "matricula_aluno[]"; // Array no PHP
+    novoInput.name = "matricula_aluno[]"; 
     novoInput.className = "form-control";
     novoInput.placeholder = "Matrícula";
 
-    // Adicionar o novo input ao div
+
     div.appendChild(novoInput);
 
-    // Adicionar o div ao container de matrículas
+
     container.appendChild(div);
 }
